e807729ba78fdd95ec02fa210258bbaa6bc3de7e
Esti Shay <esti.r.shay@gmail.com>
2017-07-14 08:42:24 -0700

    add JS to return list of numbers up to cap number provided by user

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 //Business logic
-// function numToList(userInput) {
-//
-//     return output;
-// }
+function numToList(userInput) {
+  numList = [];
+  for (i = 1; i <= userInput; i += 1) {
+    numList.push(i);
+  }
+  return numList;
+
+}
 
 
 
@@ -11,10 +15,15 @@
 
 //User Interface
 $(document).ready(function(){
+  
   $("#playing").submit(function(event){
     var input = $("input#cap-number").val();
-    // var output = numToList(input);
-    $("ol").append(input);
+    var output = numToList(input);
+
+    output.forEach(function(num){
+      $("ul").append("<li>" + num + "</li>");
+    });
+
     $("#result").show();
     event.preventDefault();
   });
